24a7e8e6f2e255059dbc71e83f5243fec625d094
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-05-18 22:13:48 +0700

    Add submitOrder()

--- app/src/main/java/com/example/myasus/justjava/MainActivity.java
+++ app/src/main/java/com/example/myasus/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myasus.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
