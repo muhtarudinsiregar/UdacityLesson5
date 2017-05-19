4dd48ceb3da44eee3c9c0985e0130e07069119ec
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-05-19 21:51:53 +0700

    Resolve error number format error low api

--- app/src/main/java/com/example/myasus/justjava/MainActivity.java
+++ app/src/main/java/com/example/myasus/justjava/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.myasus.justjava;
 
-import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
