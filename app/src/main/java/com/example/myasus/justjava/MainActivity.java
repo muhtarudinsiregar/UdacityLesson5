d1d366492ceee82c60da23d664456d31dd45e371
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-06-15 09:34:29 +0700

    Fix order button + cleanup code

--- app/src/main/java/com/example/myasus/justjava/MainActivity.java
+++ app/src/main/java/com/example/myasus/justjava/MainActivity.java
@@ -9,6 +9,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,21 +21,22 @@ public class MainActivity extends AppCompatActivity {
      * method called when increment button clicked
      * */
     public void increment(View view) {
-        int quantity = 3;
+        quantity = quantity+1;
         display(quantity);
     }
 
     /**
-     * method called when increment button clicked
+     * method called when decrement button clicked
      * */
     public void decrement(View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(1*5);
+        int totalPrice = quantity*5;
+
+        displayPrice(totalPrice);
     }
 
 
